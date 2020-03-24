b693ff2e1cb2e7524c3ff89af7d98c0b31edeb55
Ricardo <ricacor3@gmail.com>
2020-03-24 14:37:57 -0500

    se sube terminado hasta los datos por pais

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// 
 // DATOS DE COLOMBIA Y DE LAS CARDS MUNDIALES
 let cargarJSON =()=> {
     //   aqui realizamos la conecion ya sea a una archvo o a una appi
@@ -71,7 +71,7 @@ let cargarDatosPais = () => {
             let paisSinEspacio="";
             data.forEach(element => {
 
-              paisSinEspacio = (element.Country.replace(/\s/g, '-'));
+              paisSinEspacio = (element.Country);
                          
               html += `<option value="${paisSinEspacio}">${element.Country}</option>  `;
 
@@ -85,3 +85,89 @@ let cargarDatosPais = () => {
 }
 cargarJSON();
 cargarDatosPais();
+
+// DATA TABLE SEDES
+$(".tablaConsultaPais").DataTable({
+    // "ajax": "ajax/tablaSede.ajax.php", //llenamos la datatable a traves de ajax  desde este archivo
+    "deferRender": true,
+    "retrieve": true,
+    "processing": true,
+    "responsive": true,
+    "language": {
+
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando registros del _START_ al _END_",
+        "sInfoEmpty": "Mostrando registros del 0 al 0",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        }
+
+    }
+
+});
+
+$("#paises").change(function () {
+
+    
+   let pais = document.querySelector('#paises').value;
+
+   document.querySelector('.paisResultado').textContent = pais;
+ 
+   fetch('https://api.covid19api.com/summary')
+
+       .then((respuesta) => respuesta.json())
+       .then(data => {
+
+               data.Countries.forEach(element => {
+                   //dato de colombia para la portada
+                   if (element.Country == pais) {
+                      console.log(element)
+                       document.querySelector(".confirmedCountry").textContent = new Intl.NumberFormat().format(element.TotalConfirmed);
+                       document.querySelector(".deathsCountry").textContent = new Intl.NumberFormat().format(element.TotalDeaths);
+                       document.querySelector(".recoveredCountry").textContent=new Intl.NumberFormat().format(element.TotalRecovered);
+                       document.querySelector(".newConfirmed").textContent = new Intl.NumberFormat().format(element.NewConfirmed);
+                        document.querySelector('.confirmatedDate').textContent = data.Date.slice(0, -20);
+                        document.querySelector('.recoveredDate').textContent = data.Date.slice(0, -20);
+                        document.querySelector('.deathsDate').textContent = data.Date.slice(0, -20);
+                        document.querySelector('.newDate').textContent = data.Date.slice(0, -20);
+
+                   }
+ 
+               });
+           },
+           error => {
+               console.log(error);
+           })
+
+
+
+
+})
+
+    
+
+    
+
+  
+
+
+
+
+
+
